969ba9ed195d7ad43771334b3e2bd023983f6968
paladwang <rainmandavid@qq.com>
2022-10-28 11:50:42 +0800

    增加准则层权重

--- chap03/spreadsheet/pjarea.cpp
+++ chap03/spreadsheet/pjarea.cpp
@@ -2853,4 +2853,73 @@ void pjarea::debugOther() {
             m_twoZYX->setOneLevel(oneLevelSeven);
         }
     }
+
+    columnID = eColumn::w;
+    //5) 准则层权重: m_twoPower
+    m_twoPower = new column("准则层权重",columnID);
+    {
+        //1-政治环境
+        {
+            onelevel oneLevelOne("政治环境",tlevel::t1,1);
+
+            myCell curCell1(tlevel::t1,0.152207111);
+            oneLevelOne.setTwoLevel(curCell1);
+
+            m_twoPower->setOneLevel(oneLevelOne);
+        }
+        //2-油气管理体制与法律法规
+        {
+            onelevel oneLevelTwo("油气管理体制与法律法规",tlevel::t2,1);
+
+            myCell curCell1(tlevel::t1,0.152207111);
+            oneLevelTwo.setTwoLevel(curCell1);
+
+            m_twoPower->setOneLevel(oneLevelTwo);
+        }
+        //3-对外合作开放
+        {
+            onelevel oneLevelThr("对外合作开放",tlevel::t3,1);
+
+            myCell curCell1(tlevel::t1,0.152207111);
+            oneLevelThr.setTwoLevel(curCell1);
+
+            m_twoPower->setOneLevel(oneLevelThr);
+        }
+        //4-运营制度
+        {
+            onelevel oneLevelFour("运营制度",tlevel::t4,1);
+
+            myCell curCell1(tlevel::t1,0.152207111);
+            oneLevelFour.setTwoLevel(curCell1);
+
+            m_twoPower->setOneLevel(oneLevelFour);
+        }
+        //5-基础设施和自然环境
+        {
+            onelevel oneLevelFive("基础设施和自然环境",tlevel::t5,1);
+
+            myCell curCell1(tlevel::t1,0.152207111);
+            oneLevelFive.setTwoLevel(curCell1);
+
+            m_twoPower->setOneLevel(oneLevelFive);
+        }
+        //6-经济环境
+        {
+            onelevel oneLevelSix("经济环境",tlevel::t6,1);
+
+            myCell curCell1(tlevel::t1,0.150100482);
+            oneLevelSix.setTwoLevel(curCell1);
+
+            m_twoPower->setOneLevel(oneLevelSix);
+        }
+        //7-油气资源潜力
+        {
+            onelevel oneLevelSeven("油气资源潜力",tlevel::t7,1);
+
+            myCell curCell1(tlevel::t1,0.088863961);
+            oneLevelSeven.setTwoLevel(curCell1);
+
+            m_twoPower->setOneLevel(oneLevelSeven);
+        }
+    }
 }
